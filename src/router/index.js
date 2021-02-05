4802f46d043a719514b69c34bb68ec7775e16bb3
cafeway <mccafeway@gmail.com>
2021-02-05 23:39:26 +0300

    added my vue site code which is hosted on firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
+import VueSwal from 'vue-swal'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Dashboard from '../components/Dashboard'
 
 Vue.use(Router)
+Vue.use(VueSwal)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
+
+export default router

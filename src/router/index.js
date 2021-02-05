e4fe0c4a58368eb19c035ad8c1be3ffe76f07d9d
cafeway <mccafeway@gmail.com>
2021-02-06 01:12:41 +0300

    added my vue site code which is hosted on firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
-import VueSwal from '@vue-swal'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 import Dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
-Vue.use(VueSwal)
-
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,

0b83c29610f39831d8d5f30562466ac21d224871
cafeway <mccafeway@gmail.com>
2021-02-06 17:32:24 +0300

    bla bla

--- src/main.js
+++ src/main.js
@@ -3,13 +3,33 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import firebase from 'firebase'
+import 'bootstrap'
+import VueSwal from 'vue-swal'
+import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
+Vue.use(VueSwal)
 
+/* eslint no-trailing-spaces: ["error", { "skipBlankLines": true }] */
+const configOptions = {
+  apiKey: 'AIzaSyCOV7DEnom-HdWz3mO-CGtSX_at2rBZBtU',
+  authDomain: 'hustlebidders.firebaseapp.com',
+  projectId: 'hustlebidders',
+  storageBucket: 'hustlebidders.appspot.com',
+  messagingSenderId: '1045831540548',
+  appId: '1:1045831540548:web:e9be5fd96b6240c3fe4111',
+  measurementId: 'G-EQC5JY6CQ7'
+}
+firebase.initializeApp(configOptions)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  components: {App},
+  template: '<App/>',
+  methods: {
+    loginrdr: function () {
+      this.$router.push('login')
+    }
+  }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import VueSwal from 'vue-swal'
 import Router from 'vue-router'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
-import Dashboard from '@/components/Dashboard'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Dashboard from '../components/Dashboard'
 
 Vue.use(Router)
+Vue.use(VueSwal)
+
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,

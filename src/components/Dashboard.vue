884f03f5cc32243622d0a1a792b87897ec91864f
cafeway <mccafeway@gmail.com>
2021-02-08 11:20:03 +0300

    intergrating vuexfire and vuex

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,9 +5,9 @@
           <nav aria-label="breadcrumb" class="main-breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-              <li class="breadcrumb-item"><a href="javascript:void(0)">UserProfile</a></li>
-               <li class="breadcrumb-item " aria-current="page"><a><router-link :to="{ path: '/' }">Login</router-link></a></li>
-              <li class="breadcrumb-item " aria-current="page"><a><router-link :to="{ path: 'register' }">Register</router-link></a></li>
+              <li class="breadcrumb-item"><a v-on:click="redirect()" href="javascript:void(0)">Whatsapp</a></li>
+               <li class="breadcrumb-item " aria-current="page"><a v-on:click="redirecttg()"><router-link :to="{ path: '/' }">Telegram</router-link></a></li>
+               <li class="breadcrumb-item " aria-current="page"><a v-on:click="logout"><router-link :to="{ path: '/' }">Logout</router-link></a></li>
             </ol>
           </nav>
           <!-- /Breadcrumb -->
@@ -20,36 +20,13 @@
                     <div class="mt-3">
                       <h4>{{user.data.displayName}}</h4>
                       <p class="text-secondary mb-1">{{user.data.email}}</p>
-                      <p class="text-muted font-size-sm">Edit Profile</p>
-                      <button class="btn btn-primary">Edit</button>
-                      <button class="btn btn-outline-primary">Get Verified</button>
+                      <p class="text-muted font-size-sm"></p>
+                      <button class="btn btn-outline-primary">Invite Friends</button>
                     </div>
                   </div>
                 </div>
               </div>
               <div class="card mt-3">
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                    <span class="text-secondary">https://bootdey.com</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
-                    <span class="text-secondary">@bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                    <span class="text-secondary">bootdey</span>
-                  </li>
-                </ul>
               </div>
             </div>
             <div class="col-md-8">
@@ -57,7 +34,7 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col-sm-3">
-                      <h6 class="mb-0">Full Name</h6>
+                      <h6 class="mb-0">Username</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
                       {{user.data.displayName}}
@@ -88,27 +65,20 @@
                 <div class="col-sm-6 mb-3">
                   <div class="card h-100">
                     <div class="card-body">
-                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                      <small>Web Design</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Website Markup</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>One Page</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Mobile Template</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
-                      <small>Backend API</small>
-                      <div class="progress mb-3" style="height: 5px">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">buy shares</i></h6>
+                      <form>
+                         <div class="mb-3">
+    <input type="email" class="form-control" id="days"  aria-describedby="emailHelp" placeholder="number of days to the bids maturity">
+  </div>
+  <hr>
+  <div class="mb-3">
+    <input type="password" class="form-control" id="amount" placeholder="enter your bid amount">
+  </div>
+  <div class="mb-3 form-check">
+
+  </div>
+  <button type="submit" class="btn btn-success">Buy</button>
+</form>
                     </div>
                   </div>
                 </div>
@@ -154,17 +124,15 @@ export default {
       phoneNumber: ''
     }
   },
-  mounted () {
-    const user = firebase.auth().currentUser
-    const email = user.email
-    console.log(email)
-    firebase.firestore().collection('users').where('id', '==', email).get()
+  created () {
+    var db = firebase.firestore()
+    var user = firebase.auth().currentUser
+    db.collection('documents')
+      .doc(user.email)
+      .get()
       .then(snapshot => {
-        if (!snapshot.exists) {
-          console.log('NO')
-        }
-        let data = snapshot.data()
-        console.log(data)
+        const document = snapshot.data()
+        console.log(document)
       })
   },
   methods: {
@@ -173,6 +141,11 @@ export default {
     },
     redirecttg: function (event) {
       window.location = 'https://t.me/hustlerbidders'
+    },
+    logout: function () {
+      if (firebase.auth().signOut()) {
+        window.location = '/'
+      }
     }
   },
   computed: {
@@ -235,5 +208,12 @@ body{
 }
 .shadow-none {
     box-shadow: none!important;
+}
+.my-card
+{
+    position:absolute;
+    left:40%;
+    top:-20px;
+    border-radius:50%;
 }
 </style>

49b2e8dead79094c947c0534854869d75d57b3ba
cafeway <mccafeway@gmail.com>
2021-02-13 13:26:47 +0300

    i added the record page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -27,7 +27,6 @@
 
               <div class="form-group row">
                 <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
                 <div class="col-md-6">
                   <input
                     id="email"
